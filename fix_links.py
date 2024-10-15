2e51da98868e553c2f42b0a34bce201095125af5
ThorD125 <32074843+ThorD125@users.noreply.github.com>
2024-10-15 10:32:23 +0200

    Update fix_links.py

--- fix_links.py
+++ fix_links.py
@@ -1,20 +1,36 @@
 import os
 
+def add_md_extension_to_files(root_directory):
+    # Walk through all directories and subdirectories
+    for root, dirs, files in os.walk(root_directory):
+        for file in files:
+            # Check if the file has no extension
+            if '.' not in file:
+                # Add the .md extension
+                old_file_path = os.path.join(root, file)
+                new_file_path = os.path.join(root, f"{file}.md")
+                os.rename(old_file_path, new_file_path)
+
 def create_md_files(root_directory):
     # Walk through all directories and subdirectories
     for root, dirs, files in os.walk(root_directory):
         # Get the current directory name
         dir_name = os.path.basename(root)
-        # Filter and list only .md files
-        md_files = [file for file in files if file.endswith('.md')]
         
-        # Only proceed if there are .md files or subdirectories
-        if md_files or dirs:
-            # Create a filename with the format directoryname.md
+        # If we are in the root directory, set the filename to "readme.md"
+        if root == root_directory:
+            md_filename = os.path.join(root, "readme.md")
+        else:
+            # Otherwise, create a filename with the format directoryname.md
             md_filename = os.path.join(root, f"{dir_name}.md")
-            
+        
+        # Filter and list only .md and .lst files
+        relevant_files = [file for file in files if file.endswith('.md') or file.endswith('.lst')]
+        
+        # Only proceed if there are .md/.lst files or subdirectories
+        if relevant_files or dirs:
             # Create the content to be written into the markdown file
-            content = f"# {dir_name}\n\n"
+            content = f"# {dir_name if root != root_directory else 'Root Directory'}\n\n"
             
             # Add subdirectories to the content with links to their .md files
             if dirs:
@@ -22,16 +38,21 @@ def create_md_files(root_directory):
                 for directory in dirs:
                     content += f"- [{directory}]({directory}/{directory}.md)\n"
             
-            # Add the list of .md files to the content
-            if md_files:
+            # Add the list of .md and .lst files to the content
+            if relevant_files:
                 content += "\n## Files:\n"
-                for file in md_files:
+                for file in relevant_files:
                     content += f"- [{file}]({file})\n"
             
             # Write the content to the .md file
             with open(md_filename, 'w') as md_file:
                 md_file.write(content)
 
-# Specify the root directory where you want the script to start
+# Set the root directory to be the current directory (".")
 root_directory = "."
+
+# First, add .md extensions to files without extensions
+add_md_extension_to_files(root_directory)
+
+# Then, create the markdown files as described
 create_md_files(root_directory)
